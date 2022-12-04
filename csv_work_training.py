8df158a037074e4e54a71b7bb6d910296d03d61e
Rexx Egg <paco.mazerin@epsi.fr>
2022-12-04 16:42:59 +0100

    Je suis arrivé à réinitialiser la 1ère boucle for

--- csv_work_training.py
+++ csv_work_training.py
@@ -21,18 +21,19 @@ for position1 in range(len(list_to_sort)):
 #Suppression de toutes les lignes avec au moins une cellule vide :   
 
 start_of_1range = 0
-Not_out_of_range = True
+i = 0
 
-while Not_out_of_range:
-    if start_of_1range == 0:
+while i <= (len(list_to_sort)-1):
+    reset_entire_loop = False
+    i +=1
     for position1 in range(start_of_1range,len(list_to_sort)):
-        if reset_entire_loop = True:
+        if reset_entire_loop == True: 							#Nous sortons ainsi de la 1ère boucle for
             break    
         for position2 in range(len(list_to_sort[position1])):
             if list_to_sort[position1][position2] == '':
                del list_to_sort[position1]
-               start_of_1range = position1
-               reset_entire_loop = True
+               start_of_1range = position1						#Pour éviter de repartir de 0 à la première boucle for.
+               reset_entire_loop = True							#Il nous faut réinitialiser la 1ère boucle for : 1ère étape, sortir de la 1ère boucle for.
                break
 
     
