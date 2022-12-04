7092eb05aa88d42d0300ecb163e91a36a04fd980
Rexx Egg <paco.mazerin@epsi.fr>
2022-12-04 17:44:03 +0100

    J'ai trouvé un moyen de sortir de la boucle while après la suppression de toutes les lignes ne contenant pas toutes leurs cellules pleines.

--- csv_work_training.py
+++ csv_work_training.py
@@ -3,7 +3,7 @@ import csv
 
 list_to_sort = []
 #On ouvre le fichier qui nous intéresse et on l'enregistre sous la variable data_file :
-with open("conso_training.csv") as data_file:
+with open("conso-annuelles_v1.csv") as data_file:
     data = csv.reader(data_file,delimiter = '\t') #...Création de l'objet csv.reader data...
     for row in data:                              #...L'argument du paramètre 'delimiter' est important...
         list_to_sort += row                       #...Sans lui les virgules rencontrées agiraient comme des séparateurs
@@ -21,21 +21,26 @@ for position1 in range(len(list_to_sort)):
 #Suppression de toutes les lignes avec au moins une cellule vide :   
 
 start_of_1range = 0
-i = 0
+Not_out_of_range = True
 
-while i <= (len(list_to_sort)-1):
-    reset_entire_loop = False
-    i +=1
-    for position1 in range(start_of_1range,len(list_to_sort)):
-        if reset_entire_loop == True: 							#Nous sortons ainsi de la 1ère boucle for
+while Not_out_of_range:
+    reset_entire_for_loops = False
+    for position1 in range(start_of_1range,len(list_to_sort)): 
+        if reset_entire_for_loops == True: 							#Nous sortons ainsi de la 1ère boucle for.
             break    
         for position2 in range(len(list_to_sort[position1])):
-            if list_to_sort[position1][position2] == '':
-               del list_to_sort[position1]
+            if (list_to_sort[position1][position2] == '') and (position1 == len(list_to_sort)-1):
+               del list_to_sort[position1]					
+               Not_out_of_range = False            
+            elif list_to_sort[position1][position2] == '':
+               del list_to_sort[position1]						#Suppression de l'entrée ne comprenant pas 5 cellules pleines.
                start_of_1range = position1						#Pour éviter de repartir de 0 à la première boucle for.
-               reset_entire_loop = True							#Il nous faut réinitialiser la 1ère boucle for : 1ère étape, sortir de la 1ère boucle for.
+               reset_entire_for_loops = True							#Il nous faut réinitialiser la 1ère boucle for : 1ère étape, sortir de la 2ème boucle for.
                break
+            elif position1 == len(list_to_sort)-1:
+                Not_out_of_range = False
 
+            
     
     
     
