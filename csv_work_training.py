e28224bd1519792b4205d6ccc8725213400fb382
Rexx Egg <paco.mazerin@epsi.fr>
2022-12-04 20:30:49 +0100

    Bien avancé, je suis à l'étape d'addition des deux colonnes

--- csv_work_training.py
+++ csv_work_training.py
@@ -20,7 +20,9 @@ for position1 in range(len(list_to_sort)):
     list_to_sort[position1] = list_to_sort[position1].split(";")
 
 #Suppression de toutes les lignes avec au moins une cellule vide :   
-
+#Gros soucis ici : la suppression d'entrées dans notre liste nous renvoie l'erreur : 'index : out of range'...
+#...pour contourner ce problème j'ai dû faire intervenir une 3ème boucle : la boucle while.   
+    
 start_of_1range = 0
 Not_out_of_range = True
 
@@ -49,8 +51,34 @@ print(list_to_sort)
 #Suppression des colonnes ID logement :
 
 for position1 in range(len(list_to_sort)):
-    del list_to_sort[position1][1]
-
+    del list_to_sort[position1][1]								#La colonne ID logement se trouve en deuxième position dans chacune des entrées de la liste,
+                                                                #...d'où le [1].
+    
 print(len(list_to_sort))        
 print(list_to_sort)
 
+
+#Addition des consommations sur les deux années :
+#1ère étape, remplacer la virgule par un point
+
+start_of_range = 1												#On ne compte pas la première ligne : c'est le titre de chaque colonne.
+
+for position1 in range(start_of_range, len(list_to_sort)):
+    list_to_sort[position1][1] = list_to_sort[position1][1].replace(',','.')
+    list_to_sort[position1][2] = list_to_sort[position1][2].replace(',','.').replace('-','0')  #Anticipation pour la 3ème étape '-' ==> '0'
+
+print(len(list_to_sort))        
+print(list_to_sort)
+ 
+#3ème étape, convertir ces données de type string en type float
+
+start_of_range = 1												#On ne compte pas la première ligne : c'est le titre de chaque colonne.
+
+for position1 in range(start_of_range, len(list_to_sort)):
+    list_to_sort[position1][1] = float(list_to_sort[position1][1])
+    list_to_sort[position1][2] = float(list_to_sort[position1][2])
+    
+print(len(list_to_sort))        
+print(list_to_sort)
+
+#4ème étape, nous pouvons enfin additionner ces deux colonnes.
